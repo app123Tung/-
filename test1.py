2a070a8df480560507d4cb65f06cacf2a35fc481
app123Tung <tungweikai0117@gmail.com>
2026-09-07 16:09:03 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -9,7 +9,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 }
 
-token = "8719766312"
+token = "8719766312:AAH8j6gNEK1vmVnL4Yeo-W0Q7LDzUsldClg"
 chat_id = "8294056085"
 
 for stockid in stock:
